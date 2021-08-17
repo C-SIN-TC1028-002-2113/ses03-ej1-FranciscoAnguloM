96ef16bedf3e4fac6b9ccbff84afda568d2254f5
FranciscoAnguloM <a01740692@itesm.mx>
2021-08-17 14:30:03 +0000

    actiivdad de clase 3

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -1,6 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    a = (float(input("Area a pintar en metros: ")))
+    r = (float(input("Rendimiento (m2/l): ")))
+    c = (round(a/r))
+    print("Litros a comprar:",c)
 
 if __name__ == '__main__':
     main()
